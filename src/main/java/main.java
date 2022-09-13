9df6af5483b03eed465b14d0d714be95acf211a2
Dennis Baumli <baumlden@students.zhaw.ch>
2022-09-13 17:31:47 +0200

    added a main class to test if project builds as intended.

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+/**
+ * This is the java doc of this class and is used to check if the project is working properly. If this java doc comment gets deleted, checkstyle should report an error
+ */
+public class main {
+
+    /**
+     * This is the main method and is used to check if the project is working properly. If this java doc comment gets deleted, checkstyle should report an error
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World :)");
+    }
+
+}
