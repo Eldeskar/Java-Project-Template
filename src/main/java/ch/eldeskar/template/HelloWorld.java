37aeec18cf8fb572872d35247440dc8a1c973646
Dennis Baumli <baumlden@students.zhaw.ch>
2022-09-13 23:28:44 +0200

    better javadoc for HelloWorld.java

--- src/main/java/ch/eldeskar/template/HelloWorld.java
+++ src/main/java/ch/eldeskar/template/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
     /**
      * This is the main method and is used to check if the project is working properly. If this java doc comment gets deleted, checkstyle should report an error
      *
-     * @param args
+     * @param args passed to the process
      */
     public static void main(String[] args) {
         System.out.println("Hello World :)");
