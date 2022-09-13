f023c2007225303f8c12023f41b0d6a2643baf37
Dennis Baumli <baumlden@students.zhaw.ch>
2022-09-13 22:01:41 +0200

    renamed main class to HelloWorld -> main = bad class name

--- src/main/java/ch/eldeskar/template/HelloWorld.java
+++ src/main/java/ch/eldeskar/template/HelloWorld.java
@@ -1,7 +1,9 @@
+package ch.eldeskar.template;
+
 /**
  * This is the java doc of this class and is used to check if the project is working properly. If this java doc comment gets deleted, checkstyle should report an error
  */
-public class main {
+public class HelloWorld {
 
     /**
      * This is the main method and is used to check if the project is working properly. If this java doc comment gets deleted, checkstyle should report an error
